c42e8be30f68db07a6e2b127c565b0a7dba65d85
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2022-04-28 17:51:41 +0300

    Add comment

--- MusicPlayer/src/musicplayer/MusicPlayerMain.java
+++ MusicPlayer/src/musicplayer/MusicPlayerMain.java
@@ -15,7 +15,7 @@ public class MusicPlayerMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Will open the MainFrame once it's implemented (GUI)
     }
-    
+
 }
